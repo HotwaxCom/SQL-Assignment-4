70ace65154bfcfefe7ea55c879680ae8e5070171
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-08-18 15:18:39 +0530

    changed the date from created date to status date when status is SHIPMENT_SHIPPED

--- solution2.sql
+++ solution2.sql
@@ -1,12 +1,27 @@
+2.Shipments by Tracking Number
+Business Problem:
+Customer Service often needs to look up shipments by tracking number to answer delivery queries quickly.
+
+Fields to Retrieve:
+
+SHIPMENT_ID
+ORDER_ID
+TRACKING_NUMBER
+SHIPMENT_DATE
+CARRIER_PARTY_ID
+SHIPMENT_STATUS
+	
 select 
 	s.shipment_id,
 	s.primary_order_id as order_id,
 	srs.tracking_id_number as tracking_number,
-	s.created_date as shipment_date,
+	ss.status_date as shipment_date,
 	s.carrier_party_id,
 	s.status_id as shipment_status
 from shipment s
 join shipment_route_segment srs on srs.shipment_id = s.shipment_id;
+join shipment_status ss on ss.shipment_id = s.shipment_id
+	where ss.status_id = "SHIPMENT_SHIPPED";
 
 -- Here, we were required to look up for tracking number for shipments so I took basic details about shipment
 -- from the shipment table itself which includes, the shipment_id, primary_order_id, created_date, carrier_party_id
