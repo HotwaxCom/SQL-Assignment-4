6466273229347e235d0e21b51fcd51a0365bb7c4
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-08-20 11:34:58 +0530

    Replaced the function date_format to monthname

--- solution9.sql
+++ solution9.sql
@@ -1,14 +1,24 @@
+9. Shipping Revenue (Last Month)
+Business Problem:
+Finance also needs to know how much was earned from shipping charges in the same period, often a subset of overall revenue.
+
+Fields to Retrieve:
+
+TOTAL ORDER
+TOTAL_SHIPPING_REVENUE
+MONTH
+	
 select
 	count(distinct oa.order_id) as total_orders,
 	sum(oa.amount) as total_shipping_revenue,
-	date_format('2024-07-01', '%M') as month_name
+	monthname('2024-07-01') as month_name
 from
 	order_adjustment oa
 join order_header oh on
 	oh.order_id = oa.order_id
 where
 	oa.order_adjustment_type_id = 'SHIPPING_CHARGES'
-	and oh.order_date between '2024-07-01' and '2024-07-31'
+	and date(oh.order_date) between '2024-07-01' and '2024-07-31'
 group by
 	oa.order_adjustment_type_id;
 
@@ -16,4 +26,4 @@ group by
 -- total number of orders, amount as total_shipping_revenue and calculated it for the previous month. Also,
 -- ensured that order_adjustment_type_id remains "SHIPPING_CHARGES"
 
--- Execution Cost: 31,270.25
+-- Execution Cost: 62,330.7
