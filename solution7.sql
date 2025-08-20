e8a871376d09cbeba53359ac258b44e747741c4d
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-08-20 11:10:23 +0530

    Update solution7.sql

--- solution7.sql
+++ solution7.sql
@@ -1,3 +1,16 @@
+7. Single-Item Orders Fulfilled from Warehouses (Last Month)
+Business Problem:
+Identify how many single-item orders were fulfilled from warehouse facilities in the previous month, to assess picking/packing efficiency.
+
+Fields to Retrieve:
+
+ORDER_ID
+TOTAL_ORDER_ITEMS
+FACILITY_ID
+SHIPMENT_ID
+SHIPMENT_DATE
+ORDER_COMPLETION_DATE
+
 select 
     os.order_id,
     os.shipment_id,
@@ -10,17 +23,14 @@ join order_item_ship_group oisg
     on os.order_id = oisg.order_id
     and os.ship_group_seq_id = oisg.ship_group_seq_id
 join shipment_status ss 
-    on ss.shipment_id = os.shipment_id
+    on ss.shipment_id = os.shipment_id and ss.status_id = "SHIPMENT_SHIPPED"
 join order_status os1
-    on os1.order_id = os.order_id
+    on os1.order_id = os.order_id and os1.status_id = "ORDER_COMPLETED"
 join facility f
-    on f.facility_id = oisg.facility_id
+    on f.facility_id = oisg.facility_id and f.facility_type_id in ('M3_WAREHOUSE','OUTLET_WAREHOUSE','WAREHOUSE')
 WHERE 
-    ss.status_id = "SHIPMENT_SHIPPED"
-    and os1.status_id = "ORDER_COMPLETED"
-    and f.facility_type_id like '%WAREHOUSES'
-    and ss.status_date between date_format(curdate() - interval 1 month, '%Y-%m-01')
-                           AND last_day(curdate() - interval 1 month)
+	ss.status_date between date_format(curdate() - interval 1 month, '%Y-%m-01')
+	AND last_day(curdate() - interval 1 month)
 group by os.order_id, os.shipment_id, oisg.facility_id, ss.status_date, os1.status_datetime
 having 
     count(os.order_item_seq_id) = 1;
@@ -30,7 +40,7 @@ having
 -- for order_completion_date the status for order_id should be order_completed and a function was used 
 -- to get previous month's single item order fulfilled by the warehouse.
 
--- Execution Cost: 27,750.62
+-- Execution Cost: 27,312.6
 
 
 
