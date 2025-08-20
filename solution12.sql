155138930fa26c2a6de9b95c6a8fce10d0b35b14
Shreya Singh <104385986+Shreyyx@users.noreply.github.com>
2025-08-20 12:04:25 +0530

    Added the problem statement

--- solution12.sql
+++ solution12.sql
@@ -1,3 +1,13 @@
+12. List the Product’s Primary ID
+Business Problem:
+Every retailer has a specific product ID type that they throughout their systems for consistency, such as SKU or UPCA. Identify which one is used, and return the ID value for all products.
+
+Fields to Retrieve:
+
+PRODUCT_ID
+PRIMARY ID TYPE
+PRIMARY ID VALUE
+    
 select 
     p.product_id,
     gi.good_identification_type_id as PRIMARY_ID_TYPE,
@@ -5,7 +15,7 @@ select
 from product p
 join good_identification gi 
     on gi.product_id = p.product_id
-when gi.thru_date is null;
+where gi.thru_date is null;
 
 -- We were asked about the unique identification code that is used for every product and their respective
 -- values as well, so used good_identification table for this and ensured that the thru date is null which 
